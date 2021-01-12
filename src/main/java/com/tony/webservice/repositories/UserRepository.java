8138c77aa68e6d70c2137eff6e1242a43830d0f9
TonyDev77 <tony.music7@gmail.com>
2021-01-12 19:28:52 -0300

    Configurado a camada de serviço e registro de componente

--- src/main/java/com/tony/webservice/repositories/UserRepository.java
+++ src/main/java/com/tony/webservice/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.tony.webservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tony.webservice.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
