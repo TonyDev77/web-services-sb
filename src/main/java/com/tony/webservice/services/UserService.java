fed5fa5f44196eb4eb91d1b2193d539713128838
TonySilva7 <tony.deve7oper@gmail.com>
2021-02-08 11:37:04 -0300

    Exceção personalizada em findById

--- src/main/java/com/tony/webservice/services/UserService.java
+++ src/main/java/com/tony/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.tony.webservice.entities.User;
 import com.tony.webservice.repositories.UserRepository;
+import com.tony.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
---
+++ src/main/java/com/tony/webservice/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.tony.webservice.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+	
+}
